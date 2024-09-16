922faca97058dcc96a65a81484038b31ec989051
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-09-16 10:54:44 +0530

    startup code

---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./cursor.scss";
+
+const Cursor = () => {
+ 
+};
+
+export default Cursor;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,87 @@
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType:"mirror",
+      duration: 20,
+    },
+  },
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants} className="hero-heading">Shivansh Rawat</motion.h2>
+          <motion.h3 variants={textVariants}>
+            Web developer and UI designer
+          </motion.h3>
+          <motion.p variants={textVariants}><p>
+        I am a software engineer currently working at Navgurukul on an
+        e-commerce dashboard project. I am focused on automating various
+        processes such as WhatsApp automated order creation and others to
+        improve the overall efficiency of the platform.
+      </p></motion.p>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Writer Content Creator Influencer
+      </motion.div>
+      
+    </div>
+  );
+};
+
+export default Hero;
